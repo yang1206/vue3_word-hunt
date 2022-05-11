27829c6968ffa378339298d267d83b19c1310d8a
yang1206 <yang15188280673@outlook.com>
2022-05-11 14:26:11 +0800

    fix: 主题显示问题

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,5 +16,37 @@ defineEmits(['update:modelValue'])
 </script>
 
 <style lang="scss" scoped>
+header {
+  height: 30vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  h2 {
+    font-size: 7vw;
+    font-weight: 100;
+    @media (max-width: 1000px) {
+      font-size: 11vw;
+    }
+  }
 
+  .input-wrap {
+    input {
+      width: 50vw;
+      border: none;
+      // background-color: $bg;
+      border-bottom: 1px solid rgba(197, 188, 188);
+      padding: 5px;
+      color: white;
+      font-size: 18px;
+      &:focus {
+        outline: none;
+        // border-bottom: 1px solid white;
+      }
+      &::placeholder {
+        // color: rgba($color: #fff, $alpha: 0.6);
+      }
+    }
+  }
+}
 </style>
